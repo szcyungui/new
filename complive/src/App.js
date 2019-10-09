1c727e77ab4ac30e3b310cff6d09adcd05547326
Lyungui <Lyungui@outlook.com>
2019-10-09 20:40:01 +0800

    change4

---
+++ complive/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class AutoFocusInput extends Component {
+  componentDidMount () {
+    this.input.focus()
+  }
+
+  render () {
+    return (
+      <input ref={(input) => this.input = input} />
+    )
+  }
+}
+
