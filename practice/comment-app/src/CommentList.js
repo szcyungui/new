fa50087bff1e783fbd14cb032d0e55e616c2043a
Lyungui <Lyungui@outlook.com>
2019-10-09 12:35:58 +0800

    change2

--- practice/comment-app/src/CommentList.js
+++ practice/comment-app/src/CommentList.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 
 export default class CommentList extends Component {
     render() {
+        const comments = [
+          {username: 'Jerry', content: 'Hello'},
+          {username: 'Tomy', content: 'World'},
+          {username: 'Lucy', content: 'Good'}
+        ]
+    
         return (
-            <div>
-                CommentList
-            </div>
+          <div>{comments.map((comment, i) => {
+            return (
+              <div key={i}>
+                {comment.username}：{comment.content}
+              </div>
+            )
+          })}</div>
         )
-    }
+      }
 }
